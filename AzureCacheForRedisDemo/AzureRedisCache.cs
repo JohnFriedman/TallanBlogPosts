6d0a2894ae82b4b10311319a99fc43813c726294
John Friedman <john.friedman@tallan.com>
2019-10-12 14:45:05 -0400

    Add project files.

---
+++ AzureCacheForRedisDemo/AzureRedisCache.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+
+namespace AzureCacheForRedisDemo
+{
+    public class AzureRedisCache
+    {
+        private static Lazy<ConnectionMultiplexer> lazyConnection = 
+            new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect("insert connection string here"));
+
+        public static ConnectionMultiplexer Connection => lazyConnection.Value;
+
+        private static readonly string keyAppendage = "_RedisCacheKey_Dev";
+        private static readonly JsonSerializerSettings jsonSerializerSettings =
+            new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+
+        // Get entity of Type T from the Redis Cache
+        public static T GetEntity<T>(string key, Func<string, T> getFromDataStore, TimeSpan? expirationTimeSpan = null) where T : class
+        {
+            IDatabase cache = Connection.GetDatabase();
+            string entityCacheKey = key + keyAppendage;
+
+            RedisValue valueInCache = cache.StringGet(entityCacheKey);
+            T entity = string.IsNullOrWhiteSpace(valueInCache) ? null : JsonConvert.DeserializeObject<T>(valueInCache);
+
+            if(entity == null) // Not in Cache
+            {
+                // In the case of a cache miss, this retrieves the entity from the original data store
+                entity = getFromDataStore(key);
+
+                if(entity != null)
+                {
+                    cache.StringSet(entityCacheKey, JsonConvert.SerializeObject(entity, Formatting.Indented, jsonSerializerSettings));
+                    if(expirationTimeSpan != null)
+                        cache.KeyExpire(entityCacheKey, expirationTimeSpan);
+                }
+            }
+
+            return entity;
+        }
+    
+        // Delete entity from the Redis Cache
+        public static void DeleteEntity(string key)
+        {
+            var cache = Connection.GetDatabase();
+            string entityCacheKey = key + keyAppendage;
+
+            // Delete Key from Redis Cache
+            cache.KeyDelete(entityCacheKey);
+        }
+
+        // Update entity of Type T in the Redis Cache
+        public static void UpdateEntity<T>(string key, T entity, Action<T> updateEntityInDataStore, TimeSpan? expirationTimeSpan = null) where T : class
+        {
+            updateEntityInDataStore(entity);
+
+            var cache = Connection.GetDatabase();
+            string entityCacheKey = key + keyAppendage;
+
+            // Delete Key from Redis Cache
+            cache.KeyDelete(entityCacheKey);
+
+            // Set the new value to the key
+            cache.StringSet(entityCacheKey, JsonConvert.SerializeObject(entity, Formatting.Indented, jsonSerializerSettings), expirationTimeSpan);
+        }
+    }
+}
+
+
+
+        
+
---
+++ AzureCacheForRedisDemo/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AzureCacheForRedisDemo
+{
+    class Program
+    {
+        static List<Dwarf> dwarves;
+
+        static void Main(string[] args)
+        {
+            dwarves = initializeSevenDwarves();
+
+            Dwarf doc = AzureRedisCache.GetEntity("Doc", GetDwarfByName, TimeSpan.FromSeconds(5));
+            Console.WriteLine($"Name: {doc.Name}. Age: {doc.Age}. Is Happy?: {doc.IsHappy}.\n");
+
+            Dwarf grumpy = AzureRedisCache.GetEntity("Grumpy", GetDwarfByName, TimeSpan.FromSeconds(5));
+            Console.WriteLine($"Name: {grumpy.Name}. Age: {grumpy.Age}. Is Happy?: {grumpy.IsHappy}.\n");
+
+            Console.WriteLine("Now it's Grumpy's Birthday!");
+            AzureRedisCache.UpdateEntity(grumpy.Name, grumpy, TriggerDwarfBirthday, TimeSpan.FromSeconds(5));
+            Console.WriteLine($"Name: {grumpy.Name}. Age: {grumpy.Age}. Is Happy?: {grumpy.IsHappy}.");
+            Console.ReadKey();
+        }
+
+        static Dwarf GetDwarfByName(string name)
+        {
+            return dwarves.FirstOrDefault(d => d.Name == name);
+        }
+
+        static void TriggerDwarfBirthday(Dwarf dwarf)
+        {
+            dwarf.Age++;
+            dwarf.IsHappy = true;
+        }
+
+        class Dwarf
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public bool IsHappy { get; set; }
+        }
+
+        static List<Dwarf> initializeSevenDwarves()
+        {
+            return new List<Dwarf>
+            {
+                new Dwarf
+                {
+                    Name = "Bashful",
+                    Age = 80,
+                    IsHappy = true
+                },
+                new Dwarf
+                {
+                    Name = "Doc",
+                    Age = 85,
+                    IsHappy = true
+                },
+                new Dwarf
+                {
+                    Name = "Grumpy",
+                    Age = 99,
+                    IsHappy = false
+                },
+                new Dwarf
+                {
+                    Name = "Happy",
+                    Age = 77,
+                    IsHappy = true
+                },
+                new Dwarf
+                {
+                    Name = "Sneezy",
+                    Age = 84,
+                    IsHappy = true
+                },
+                new Dwarf
+                {
+                    Name = "Sleepy",
+                    Age = 98,
+                    IsHappy = false
+                },
+                new Dwarf
+                {
+                    Name = "Dopey",
+                    Age = 50,
+                    IsHappy = false
+                }
+            };
+        }
+    }
+}
